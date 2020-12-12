b30ede88d2ddac11a8cfc99dd51102d497475395
samueldcorbin <samueldcorbin@gmail.com>
2020-12-12 07:52:19 -0500

    No need for bitbucket

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,8 +13,7 @@ if [ ! -f "$HOME/.ssh/id_rsa" ]; then
     echo "Public key:"
     cat "$HOME/.ssh/id_rsa.pub"
     echo "GitHub:    https://github.com/settings/keys"
-    echo "Bitbucket: https://bitbucket.org/account/user/samueldcorbin/ssh-keys/"
-    read -n 1 -p "Add key to GitHub/Bitbucket, then press any key to continue. "
+    read -n 1 -p "Add key to GitHub, then press any key to continue. "
     echo ""
 fi
 
